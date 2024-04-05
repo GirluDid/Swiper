903e228782809f2cfba47e3e0cefeff760d2aef3
GirluDid <153798458+GirluDid@users.noreply.github.com>
2024-04-05 10:53:41 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",},
+      pagination: {
+        el: ".swiper-pagination",},
+      mousewheel: true,
+      keyboard: true,});
